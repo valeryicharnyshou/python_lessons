9672c89a72abd94bbfe4fd7308111f84b1256feb
Valeryi Charnyshou <vcharnyshou@icloud.com>
2020-12-24 12:39:15 +0100

    Task 1 completed

--- 1.py
+++ 1.py
@@ -1,1 +1,10 @@
-#py test file
+a = int(17)  # int
+b = str("some_text")  # str
+c = float(17.1)  # float
+d = True  # bool
+print(a, b, c, d)
+e = int(input("Введите первое число: "))
+f = int(input("Введите второе число: "))
+print("Сумма введенных чисел равна:", (e + f))
+username = input("Введите Ваше имя: ")
+print("Вас зовут: " + username)
