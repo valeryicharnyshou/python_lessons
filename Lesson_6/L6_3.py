f30e85bb52118886cdf295da53c5839c27dbefdb
Valeryi Charnyshou <vcharnyshou@icloud.com>
2021-01-25 14:21:39 +0100

    Lesson6 Task3

---
+++ Lesson_6/L6_3.py
@@ -0,0 +1,25 @@
+class Worker:
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position, wage, bonus):
+        Worker.__init__(self, name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_total_income(self):
+        return self._income.get('wage') + self._income.get('bonus')
+
+
+tester = Position('Ivan', 'Ivanov', 'Manager', 150000, 50000)
+print(tester.get_full_name())
+print(tester.position)
+print(tester.get_total_income())
